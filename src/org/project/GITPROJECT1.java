57ade60a59476c0281f3c5032adff85f39528c9d
Archana1507 <archana123ss@gmail.com>
2021-05-07 12:33:47 +0530

    added to AB

--- src/org/project/GITPROJECT1.java
+++ src/org/project/GITPROJECT1.java
@@ -5,6 +5,16 @@ public class GITPROJECT1 {
 	public static void main(String[] args) {
 		String name="Archana";
 		System.out.println(name);
+		int a=89;
+		System.out.println(a);
 	}
 
 }
+
+
+
+
+
+
+
+// Local--------Branch-----From branch create reuest
